48ea14a5fc5b08ce62b1dffb26a0f6bba61c5832
luiznaiper <luiz_naiper@hotmail.com>
2022-05-24 07:55:05 -0300

    :construction: Added axios and prepared, API not working

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -4,17 +4,29 @@ import Home from '../containers/Home';
 import Checkout from '../containers/Checkout';
 import Layout from '../components/Layout';
 import NotFound from '../containers/NotFound';
+import { useInitialState } from '../hooks/useInitialState';
 
-const App = () => (
-  <BrowserRouter>
-    <Layout>
-      <Routes>
-        <Route exact path='/' element={<Home />} />
-        <Route exact path="/checkout" component={Checkout} />
-        <Route path ='*' element={<NotFound />} />
-      </Routes>
-    </Layout>
-  </BrowserRouter>
-);
+const App = () => {
+  const initialState = useInitialState()
+  const isEmpty = Object.keys(initialState.products).length
+  return(
+    <React.Fragment>
+        {isEmpty > 0 ? (
+          <BrowserRouter>
+          <Layout>
+            <Routes>
+              <Route exact path='/' element={<Home />} />
+              <Route exact path="/checkout" component={Checkout} />
+              <Route path ='*' element={<NotFound />} />
+            </Routes>
+          </Layout>
+        </BrowserRouter>
+        ) : <h1>Loading...</h1>
+      }
+    </React.Fragment>
+    
+  )
+}
+  
 
 export default App;
